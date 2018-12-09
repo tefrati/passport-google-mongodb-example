1975985faccce1e76bd9d94634e44b51c1809b58
Tzahi Efrati <tzahi.efrati@gmail.com>
2018-12-08 23:14:19 -0500

    using one client and connecting to all collections at startup, closing on SIGINT

--- app/Database/mongodb.js
+++ app/Database/mongodb.js
@@ -2,14 +2,26 @@
 const {MongoClient, ObjectId} = require("mongodb")
 
 const config = require("../../config/db")[process.env.NODE_ENV]
+let client, db, collections = {}
 
-const getCollection = async (name) => {
-	let client = new MongoClient(config.connection, { useNewUrlParser: true })
+;(async () => {
+	client = new MongoClient(config.connection, { useNewUrlParser: true })
 	await client.connect()
-	let db = client.db()
-	const collection = config.collections[name]
-	db.createCollection(collection.name, collection.options)
-	return db.collection(collection.name)
+	db = client.db()
+	for (let name in config.collections) {
+		db.createCollection(name, config.collections[name].options)
+		collections[name] = db.collection(name)
+	}
+})()
+
+const getCollection = (name) => {
+	return collections[name]
 }
 
-module.exports = {getCollection, ObjectId}
+// listen for the signal interruption (ctrl-c)
+process.on("SIGINT", () => {
+	client.close()
+	process.exit()
+})
+
+module.exports = {getCollection}
